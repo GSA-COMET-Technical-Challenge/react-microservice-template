e8c18d6ac1a5cfe99bc9094691276ce140520ac0
Yogesh Asthana <yogesh.asthana@inadev.com>
2021-10-28 20:50:09 +0530

    search bar config

---
+++ src/config/searchbar.js
@@ -0,0 +1,19 @@
+let search_bar_config = 
+{
+    "fields": [
+        {
+            "component": "text",
+            "label": "Field 1",
+            "type": "text",
+            "id": "1"
+        },
+        {
+            "component": "text",
+            "label": "Field 2",
+            "type": "text",
+            "id": "2"
+        }
+    ]
+}
+
+export default search_bar_config;
